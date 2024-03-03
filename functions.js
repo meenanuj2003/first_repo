e8c7587dba171872c0cbb6bfe5c8eb7ab656d6ab
Anuj Meena <127922671+meenanuj2003@users.noreply.github.com>
2024-03-03 22:43:16 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,25 @@
+function nice(name) {
+    console.log("your name is " + name ) 
+    console.log( name + " you are a good boy " ) 
+    console.log(name + " is from jaipur") 
+}
+
+
+nice("Anuj")
+
+
+
+
+sum(1,2)
+
+function sum(a,b) {
+    console.log(a+b)
+}
+
+
+var a = ("enter value of a ")
+var b = ("enter value of b ")
+
+
+
+sum(a,b)
